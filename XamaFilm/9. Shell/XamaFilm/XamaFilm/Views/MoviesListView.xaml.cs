f8107cd8b56fa4b725d630194757b3d1207a03a4
info@riojadotnet.com <asiertarancon@outlook.com>
2019-07-01 00:02:05 +0200

    First commit

---
+++ XamaFilm/9. Shell/XamaFilm/XamaFilm/Views/MoviesListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamaFilm.Services;
+using XamaFilm.ViewModels;
+using XamaFilm.ViewModels.Base;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamaFilm.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MoviesListView : ContentPage
+	{
+        ViewModelBase VM;
+        public MoviesListView ()
+		{
+			InitializeComponent ();
+
+            VM = new MoviesListViewModel(DependencyService.Get<IDataService>());
+            BindingContext = VM;
+        }
+
+        protected override void OnAppearing()
+        {
+            VM.InitializeAsync(null);
+        }
+    }
+}
---
+++ XamaFilm/9. Shell/XamaFilm/XamaFilm/Views/SeriesListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamaFilm.Services;
+using XamaFilm.ViewModels;
+using XamaFilm.ViewModels.Base;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamaFilm.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SeriesListView : ContentPage
+	{
+        ViewModelBase VM;
+		public SeriesListView ()
+		{
+			InitializeComponent ();
+
+            VM = new SeriesListViewModel(DependencyService.Get<IDataService>());
+            BindingContext = VM;
+		}
+
+        protected override void OnAppearing()
+        {
+            VM.InitializeAsync(null);
+        }
+    }
+}
