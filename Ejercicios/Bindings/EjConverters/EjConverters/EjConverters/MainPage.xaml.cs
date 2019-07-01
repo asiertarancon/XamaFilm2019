a3e02ca58b11ad102ab8c13caa2f9167f97e8691
info@riojadotnet.com <asiertarancon@outlook.com>
2019-07-02 00:09:55 +0200

    Añade ejercicio converters

---
+++ Ejercicios/Bindings/EjConverters/EjConverters/EjConverters/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EjConverters
+{
+    public enum Semaphore
+    {
+        green,
+        yellow,
+        red
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private Semaphore semaphoreValue;
+        public Semaphore SemaphoreValue
+        {
+            get => semaphoreValue;
+            set
+            {
+                semaphoreValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (semaphoreValue == Semaphore.green)
+                SemaphoreValue = Semaphore.yellow;
+            else if (semaphoreValue == Semaphore.yellow)
+                SemaphoreValue = Semaphore.red;
+            else
+                SemaphoreValue = Semaphore.green;
+
+
+        }
+    }
+}
---
+++ Ejercicios/Bindings/EjConverters/EjConverters/EjConverters/SemaphoreConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EjConverters
+{
+    public class SemaphoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((Semaphore)value)
+            {
+                case Semaphore.green: return Color.Green;
+                case Semaphore.yellow: return Color.Yellow;
+                case Semaphore.red: return Color.Red;
+                default:
+                    return Color.Black;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
