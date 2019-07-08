9589c46284148ce5f4d1e09db4a55a2f55857784
info@riojadotnet.com <asiertarancon@outlook.com>
2019-07-09 01:11:37 +0200

    Añade localización a XamaFilm

---
+++ XamaFilm/10. Localizacion/XamaFilm/XamaFilm/ViewModels/AcknowledgementViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using XamaFilm.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace XamaFilm.ViewModels
+{
+    public class AcknowledgementViewModel : ViewModelBase
+    {
+        public AcknowledgementViewModel()
+        {
+            Licenses = new ObservableCollection<License>(new List<License>()
+            {
+                new License("Json.NET", "The MIT License", "https://www.newtonsoft.com/json"),
+                new License("Xamarin Forms", "The MIT License", "https://www.xamarin.com/forms"),
+                new License("Xamarin Forms Maps", "The MIT License", "https://www.xamarin.com/forms"),
+            });
+        }
+
+        private ObservableCollection<License> _licenses;
+        public ObservableCollection<License> Licenses
+        {
+            get => _licenses;
+            set => SetProperty(ref _licenses, value);
+        }
+
+        public ICommand ItemSelectedCommand => new Command<License>(OpenURL);
+
+        public void OpenURL(License license)
+        {
+            Device.OpenUri(new Uri(license.URL));
+        }
+    }
+
+    public class License : BindableObject
+    {
+        public string Title { get; set; }
+        public string LicenseType { get; set; }
+        public string URL { get; set; }
+
+        public License(string title, string type, string url)
+        {
+            Title = title;
+            LicenseType = type;
+            URL = url;
+        }
+    }
+}
---
+++ XamaFilm/10. Localizacion/XamaFilm/XamaFilm/Views/MoviesListView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamaFilm.Models;
+using XamaFilm.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamaFilm.Views
+{
+    public partial class MoviesListView : ContentPage
+    {
+        public MoviesListView()
+        {
+            InitializeComponent();
+
+            BindingContext = new MoviesViewModel();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (listview.SelectedItem == null)
+                return;
+            Navigation.PushAsync(new DetailMovieView(listview.SelectedItem as Movie));
+            listview.SelectedItem = null;
+        }
+    }
+}
