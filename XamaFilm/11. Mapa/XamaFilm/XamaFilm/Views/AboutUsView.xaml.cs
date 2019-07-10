2bdd4d4ab7072378bb5f888d1912df8c020df668
Asier Tarancón <asiertarancon@azkoyen.com>
2019-07-10 16:08:26 +0200

    Modificado mapa para que se centre en pantalla

--- XamaFilm/11. Mapa/XamaFilm/XamaFilm/Views/AboutUsView.xaml.cs
+++ XamaFilm/11. Mapa/XamaFilm/XamaFilm/Views/AboutUsView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using XamaFilm.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace XamaFilm.Views
@@ -15,6 +16,8 @@ namespace XamaFilm.Views
 		{
 			InitializeComponent ();
 			BindingContext = new AboutUsViewModel();
+
+			MapSpan.FromCenterAndRadius(new Position(42.461108, -2.427075), new Distance(10000));
 		}
 	}
 }
